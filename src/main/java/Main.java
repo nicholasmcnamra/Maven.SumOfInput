7c0bcac1b7f5ba4ffcfc65d2352e8bb4fa57a3a7
nicholasmcnamra <nikomcnamara@gmail.com>
2024-02-28 18:07:32 -0500

    lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
@@ -7,21 +8,35 @@ public class Main {
     public static void main(String[] args){
         
         int number = getInputNumber();
-        
-        int sum = sumOfNumbers(number);
+        long sumStartTime = System.nanoTime();
+        // I worked with Jake on the extra credit part of this one and he had the great suggestion to use System.nanoTime();!
+        long sum = sumOfNumbers(number);
+        long sumElapsed = System.nanoTime() - sumStartTime;
+        System.out.println(sumElapsed + "ns");
         
         System.out.println(sum);
 
+        long gaussStartTime = System.nanoTime();
+        long gaussianSum = gaussianSumOfNumbers(number);
+        long gaussElapsed = System.nanoTime() - gaussStartTime;
+        System.out.println(gaussElapsed + "ns");
+
+        System.out.println(gaussianSum);
     }
+
+
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static long sumOfNumbers(int n) {
+        long temp = 0;
+        for (int i = 0; i <= n; i++) {
+            temp += i;
+        }
+        return temp;
     }
-    
-    
-    int getInputNumber() { // gee, may thi sis useful in another lab?
+
+    static int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -36,9 +51,12 @@ public class Main {
     
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static long gaussianSumOfNumbers(int n) {
+        //gaussians sum equation is the result (n * (n +1)) / 2
+        long sum = ((long) n * ((long) n + 1)) / 2;
+        return sum;
     }
+
    
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
